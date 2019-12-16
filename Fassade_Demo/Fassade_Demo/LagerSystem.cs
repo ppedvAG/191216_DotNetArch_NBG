d32bfbc8a3478f2797cf74a61dd0f61594759bb4
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-16 14:18:24 +0100

    Variante ohne Fassade

---
+++ Fassade_Demo/Fassade_Demo/LagerSystem.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fassade_Demo
+{
+    class LagerSystem
+    {
+        public bool IstProduktLagernd(int ProduktID)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Thread.Sleep(1000);
+            bool result = false;
+            if (ProduktID % 2 == 0)
+            {
+                Console.WriteLine($"Das Produkt mit der ID {ProduktID} ist lagernd");
+                result =  true;
+            }
+            else
+            {
+                Console.WriteLine($"Das Produkt mit der ID {ProduktID} ist nicht lagernd");
+                result = false;
+            }
+            Console.ResetColor();
+            return result;
+        }
+
+        public void ProduktNachbestellen(int ProduktID)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Thread.Sleep(2500);
+            Console.WriteLine($"Das Produkt mit der ID {ProduktID} wurde soeben nachbestellt ....");
+            Console.ResetColor();
+        }
+    }
+
+    class RechnungsSystem
+    {
+        public bool HatOffeneRechnungen(int KundenID)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Thread.Sleep(500);
+            bool result = false;
+            if (KundenID % 5 == 3)
+            {
+                Console.WriteLine($"Rechnungen sind noch offen, Vorgang wird abgebrochen");
+                result = true;
+            }
+            else
+            {
+                Console.WriteLine($"Alles OK");
+                result = false;
+            }
+            Console.ResetColor();
+            return result;
+        }
+
+        public void BezahlvorgangStarten(int KundenID)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Bezahlvorgang wird gestartet....");
+            Thread.Sleep(2500);
+            Console.WriteLine($"Bezahlvorgang Erfolgreich !");
+            Console.ResetColor();
+        }
+    }
+
+    class EmailSystem
+    {
+        public void BestätigungsmailVersenden()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Bestätigungsmail wird versendet....");
+            Thread.Sleep(500);
+            Console.WriteLine("Bestätigungsmail wurde versendet !");
+            Console.ResetColor();
+        }
+    }
+
+    class DHLVersandSystem
+    {
+        public void VersendeProdukt()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Das Produkt befindet sich auf dem Versandweg");
+            Thread.Sleep(10000);
+            Console.WriteLine("Empfänger ist nicht zuhause, Paket wird in der Abholstation abgelegt");
+            Console.ResetColor();
+        }
+    }
+}
--- Fassade_Demo/Fassade_Demo/Program.cs
+++ Fassade_Demo/Fassade_Demo/Program.cs
@@ -10,6 +10,40 @@ namespace Fassade_Demo
     {
         static void Main(string[] args)
         {
+            // Hauptsystem -> In der Webseite der Button "Bestellen"
+            Console.OutputEncoding = Encoding.Unicode;
+
+            LagerSystem ls = new LagerSystem();
+            RechnungsSystem rs = new RechnungsSystem();
+            DHLVersandSystem vs = new DHLVersandSystem();
+            EmailSystem es = new EmailSystem();
+
+
+
+            // Ohne Fassade:
+
+            Console.WriteLine("Bitte geben Sie den zu Bestellenden Artikel ein:");
+            Console.WriteLine("Artikel 1: GoF - Designpatterns - Buch 49,99€");
+            Console.WriteLine("Artikel 2: FireTV Stick 69,99€");
+            Console.WriteLine("Artikel 3: Romantische Teelichter - 0,99€");
+
+            int artikelnummer = Convert.ToInt32(Console.ReadLine());
+
+            if(rs.HatOffeneRechnungen(122) == false)
+            {
+                if(ls.IstProduktLagernd(artikelnummer))
+                {
+                    rs.BezahlvorgangStarten(122);
+                    es.BestätigungsmailVersenden();
+                    vs.VersendeProdukt();
+                }
+                else
+                {
+                    Console.WriteLine("Das Produkt ist in ihrem Land nicht verfügbar");
+                }
+            }
+            else
+                Console.WriteLine("BEZAHL ERST DEINEN DECKEL !!!!!!!");
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
