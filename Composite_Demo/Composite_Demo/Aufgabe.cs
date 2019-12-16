e5c07b6bdf37d0a0a4675a600ba1725ca571a1ad
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-16 14:59:34 +0100

    Baumstruktur mit Aufgaben

---
+++ Composite_Demo/Composite_Demo/Aufgabe.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite_Demo
+{
+    public abstract class Aufgabe
+    {
+        public string Titel { get; set; }
+        public abstract TimeSpan Dauer { get; set; }
+        public abstract bool IstErledigt { get; set; }
+    }
+
+    public class Einzelaufgabe : Aufgabe
+    {
+        public string Person { get; set; }
+        public override bool IstErledigt { get; set; }
+        public override TimeSpan Dauer { get; set; }
+    }
+
+    public class Aufgabenliste : Aufgabe
+    {
+        public List<Aufgabe> Unteraufgaben { get; set; } = new List<Aufgabe>();
+
+        public string[] Personen
+        {
+            get => Unteraufgaben.Where(x => x is Einzelaufgabe)
+                                .Cast<Einzelaufgabe>()
+                                .Select(x => x.Person)
+                                .Distinct()
+                                .ToArray();
+        }
+        public override bool IstErledigt
+        {
+            get
+            {
+                return Unteraufgaben.All(x => x.IstErledigt == true);
+            }
+            set
+            {
+                foreach (Aufgabe unerledigt in Unteraufgaben.Where(x => x.IstErledigt == false))
+                {
+                    unerledigt.IstErledigt = true;
+                }
+            }
+        }
+
+        public override TimeSpan Dauer
+        {
+            get
+            {
+               return TimeSpan.FromSeconds(Unteraufgaben.Select(x => x.Dauer.TotalSeconds)
+                                                        .Sum());
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+}
--- Composite_Demo/Composite_Demo/Program.cs
+++ Composite_Demo/Composite_Demo/Program.cs
@@ -10,6 +10,38 @@ namespace Composite_Demo
     {
         static void Main(string[] args)
         {
+            Aufgabenliste einkaufen = new Aufgabenliste();
+            einkaufen.Unteraufgaben.Add(new Einzelaufgabe { Person = "Michi Z", Titel = "Äpfel", Dauer = TimeSpan.FromMinutes(3), IstErledigt = false });
+            einkaufen.Unteraufgaben.Add(new Einzelaufgabe { Person = "Michi Z", Titel = "Birnen", Dauer = TimeSpan.FromMinutes(1), IstErledigt = false });
+            einkaufen.Unteraufgaben.Add(new Einzelaufgabe { Person = "Michi Z", Titel = "Bananen", Dauer = TimeSpan.FromMinutes(5), IstErledigt = true });
+
+            Aufgabenliste haushalt = new Aufgabenliste();
+            haushalt.Unteraufgaben.Add(new Einzelaufgabe { Person = "Michi Z", Titel = "Staubsaugen", Dauer = TimeSpan.FromMinutes(15), IstErledigt = true });
+            haushalt.Unteraufgaben.Add(new Einzelaufgabe { Person = "Michi Z", Titel = "Kochen", Dauer = TimeSpan.FromMinutes(30), IstErledigt = true });
+            haushalt.Unteraufgaben.Add(new Einzelaufgabe { Person = "Hund", Titel = "Gassi gehen", Dauer = TimeSpan.FromMinutes(60), IstErledigt = true });
+
+            Aufgabenliste tagesaufgaben = new Aufgabenliste();
+            tagesaufgaben.Unteraufgaben.Add(einkaufen);
+            tagesaufgaben.Unteraufgaben.Add(haushalt);
+            tagesaufgaben.Unteraufgaben.Add(new Einzelaufgabe { Person = "Hund", Titel = "Wild durch die Wohnung rennen", Dauer = TimeSpan.FromMinutes(120), IstErledigt = true });
+
+
+            if(tagesaufgaben.IstErledigt)
+            {
+                Console.WriteLine("Alle Tagesaufgaben sind erledigt");
+            }
+
+            if(haushalt.IstErledigt)
+            {
+                Console.WriteLine("Zuhause gibts nichts mehr zu tun :) ");
+                Console.WriteLine(haushalt.Dauer.TotalMinutes);
+            }
+
+            Console.WriteLine(tagesaufgaben.Dauer.TotalMinutes);
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
