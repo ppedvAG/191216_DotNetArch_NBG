b34b904270c3502b0d219ee6277b31e475faa7ec
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-16 11:36:39 +0100

    Threadsicheres Singleton

---
+++ Singleton_Demo/Singleton_Demo/Logger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Singleton_Demo
+{
+    class Logger
+    {
+        private Logger()
+        {
+            instanceCounter++;
+        }
+
+        private static object lockObject = new object();
+        private static int instanceCounter;
+        private static Logger instance;
+        public static Logger Instance
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        Thread.Sleep(100);
+                        instance = new Logger();
+                    }
+                }
+             
+
+                return instance;
+            }
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"{instanceCounter}[{DateTime.Now}]: {message}");
+        }
+    }
+}
--- Singleton_Demo/Singleton_Demo/Program.cs
+++ Singleton_Demo/Singleton_Demo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Singleton_Demo
@@ -10,6 +11,21 @@ namespace Singleton_Demo
     {
         static void Main(string[] args)
         {
+            //new Logger().Log("Hallo Welt");
+            //new Logger().Log("Demo  1234");
+            //new Logger().Log("dadadadadadadaadaaaaa batmaaaan");
+
+            Parallel.For(0, 10_000_000, index =>
+             {
+                 //Logger log2 = new Logger();
+                 //log2.Log($"Batman Nr {index}# meldet sich !!!!");
+                 Logger.Instance.Log($"Batman Nr {index}# meldet sich !!!!");
+             });
+
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
