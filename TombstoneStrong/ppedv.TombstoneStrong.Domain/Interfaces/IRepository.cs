aaa517946acd62dbc66f5e91f51b0bcd1eac240e
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-17 09:53:42 +0100

    Interface für das Repository

---
+++ TombstoneStrong/ppedv.TombstoneStrong.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ppedv.TombstoneStrong.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        void Add<T>(T item) where T : Entity;
+        void Delete<T>(T item) where T : Entity;
+        void Update<T>(T item) where T : Entity;
+        T GetByID<T>(int ID) where T : Entity;
+        IEnumerable<T> GetAll<T>() where T : Entity;
+        IQueryable<T> Query<T>() where T : Entity; // Für LINQ auf dem DBSet
+        void Save();
+    }
+}
