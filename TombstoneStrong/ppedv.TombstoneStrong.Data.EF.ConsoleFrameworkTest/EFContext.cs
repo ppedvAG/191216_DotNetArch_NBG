05b5217aef94c6e750e2801f9ed83dee4f19f2c6
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-17 15:27:12 +0100

    Test mit EF 6.2

---
+++ TombstoneStrong/ppedv.TombstoneStrong.Data.EF.ConsoleFrameworkTest/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using ppedv.TombstoneStrong.Domain;
+using System.Data.Entity;
+
+namespace ppedv.TombstoneStrong.Data.EF.ConsoleFrameworkTest
+{
+    class EFContext : DbContext
+    {
+        // Trainer:
+        //public EFContext() : base(@"Server=(localDb)\MSSQLLocalDb;Database=TombstoneTEST;Trusted_Connection=true")
+        public EFContext() : base(@"Server=.;Database=TombstoneTEST;Trusted_Connection=true")
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<TimeSheet> TimeSheet { get; set; }
+    }
+}
---
+++ TombstoneStrong/ppedv.TombstoneStrong.Data.EF.ConsoleFrameworkTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.TombstoneStrong.Data.EF.ConsoleFrameworkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EFContext context = new EFContext();
+
+            var item = context.Employee.FirstOrDefault(x => x.ID == 1);
+            Console.WriteLine(item?.Name ?? "Kein Item gefunden");
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
