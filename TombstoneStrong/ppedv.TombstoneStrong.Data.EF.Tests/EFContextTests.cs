c64f7433845fd975e0667f6f326912c6431474e8
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-17 09:50:14 +0100

    EFContext - Verbindung getestet

---
+++ TombstoneStrong/ppedv.TombstoneStrong.Data.EF.Tests/EFContextTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.TombstoneStrong.Data.EF.Tests
+{
+    [TestFixture]
+    public class EFContextTests
+    {
+        [Test]
+        public void CanCreateEFContext()
+        {
+            using (EFContext context = new EFContext())
+            {
+                // ...
+            }
+        }
+
+        [Test]
+        public void EFContext_Can_Create_DB()
+        {
+            using (EFContext context = new EFContext())
+            {
+                Assert.IsTrue(context.Database.EnsureCreated());
+            }
+        }
+    }
+}
