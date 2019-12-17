4abe53e0909fc9efd7adcc7a6964a7889720cf84
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-17 09:43:31 +0100

    Minimalkonfiguration des EFCore

---
+++ TombstoneStrong/ppedv.TombstoneStrong.Data.EF/EFContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.TombstoneStrong.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ppedv.TombstoneStrong.Data.EF
+{
+    public class EFContext : DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<TimeSheet> TimeSheet { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Für Trainer-Rechner: 
+            optionsBuilder.UseSqlServer(@"Server=(localDb)\MSSQLLocalDb;Database=TombstoneStrong;Trusted_Connection=true");
+            // Für TN-Rechner : optionsBuilder.UseSqlServer(@"Server=.")
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
