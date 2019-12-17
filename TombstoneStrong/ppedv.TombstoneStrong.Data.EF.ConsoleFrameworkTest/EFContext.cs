a8b9305d0f0226cc9a71c2ba00bde459be9fa06e
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-17 15:50:47 +0100

    Verbindung auf ehem. EF Core- DB

--- TombstoneStrong/ppedv.TombstoneStrong.Data.EF.ConsoleFrameworkTest/EFContext.cs
+++ TombstoneStrong/ppedv.TombstoneStrong.Data.EF.ConsoleFrameworkTest/EFContext.cs
@@ -1,5 +1,6 @@
 ﻿using ppedv.TombstoneStrong.Domain;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ppedv.TombstoneStrong.Data.EF.ConsoleFrameworkTest
 {
@@ -7,8 +8,13 @@ namespace ppedv.TombstoneStrong.Data.EF.ConsoleFrameworkTest
     {
         // Trainer:
         //public EFContext() : base(@"Server=(localDb)\MSSQLLocalDb;Database=TombstoneTEST;Trusted_Connection=true")
-        public EFContext() : base(@"Server=.;Database=TombstoneTEST;Trusted_Connection=true")
+        public EFContext() : base(@"Server=.;Database=TombstoneStrong;Trusted_Connection=true")
         {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
 
         public DbSet<Employee> Employee { get; set; }
