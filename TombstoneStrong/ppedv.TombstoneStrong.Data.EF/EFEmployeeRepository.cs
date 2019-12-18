c88202b9f1e72eb8d417dd699a44d4d7be30b25f
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-18 10:01:53 +0100

    EFUnitOfWork erstellt

---
+++ TombstoneStrong/ppedv.TombstoneStrong.Data.EF/EFEmployeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using ppedv.TombstoneStrong.Domain;
+using ppedv.TombstoneStrong.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ppedv.TombstoneStrong.Data.EF
+{
+    public class EFEmployeeRepository : EFUniversalRepository<Employee>, IEmployeeRepository
+    {
+        public EFEmployeeRepository(EFContext context) : base(context){}
+
+        public TimeSheet[] GetAllTimeSheetsForEmployee(Employee input)
+        {
+            return context.TimeSheet.Where(x => x.Employee.ID == input.ID).ToArray();
+        }
+
+        public Employee GetEmployeeWithMostWorkhours()
+        {
+            // ToDO: alles zu einem einzigen LINQ - Befehl verarbeiten 😎
+            double maxWorkHours = 0;
+            Employee result = null;
+
+            foreach (Employee employee in context.TimeSheet.Select(x => x.Employee)
+                                                           .Distinct())
+            {
+                var totalWorkHours = GetAllTimeSheetsForEmployee(employee).Sum(x => (x.End - x.Start).TotalMinutes);
+                if(totalWorkHours > maxWorkHours)
+                {
+                    maxWorkHours = totalWorkHours;
+                    result = employee;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TombstoneStrong/ppedv.TombstoneStrong.Data.EF/EFUnitOfWork.cs
@@ -0,0 +1,61 @@
+﻿using ppedv.TombstoneStrong.Domain;
+using ppedv.TombstoneStrong.Domain.Interfaces;
+
+namespace ppedv.TombstoneStrong.Data.EF
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        #region Variante 1:
+        //public EFUnitOfWork(EFContext context)
+        //{
+        //    this.context = context;
+        //}
+        //private readonly EFContext context; 
+        #endregion
+
+        // Variante 2: Context als Singleton
+        private readonly object lockObject = new object();
+        private EFContext context;
+        private EFContext Context
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (context == null)
+                        context = new EFContext();
+                }
+                return context;
+            }
+        }
+
+
+        private IEmployeeRepository employeeRepository;
+        public IEmployeeRepository EmployeeRepository
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (employeeRepository == null)
+                        employeeRepository = new EFEmployeeRepository(Context);
+                }
+
+                return employeeRepository;
+            }
+        }
+
+        public IUniversalRepository<T> GetRepository<T>() where T : Entity
+        {
+            // ToDo: Pro-Variante: Wenn T ein Employee ist -> EmployeeRepository zurückgeben
+            // ToDo: Repository-Cache, damit nichts doppelt generiert wird
+            return new EFUniversalRepository<T>(Context);
+        }
+
+        public void SaveAll()
+        {
+            if (Context.ChangeTracker.HasChanges())
+                Context.SaveChanges();
+        }
+    }
+}
